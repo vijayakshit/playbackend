7c04604da8cc24986b3ddd7c2043294329c42918
Akshit Vijay <akshitthevijay@gmail.com>
2018-07-14 15:36:47 +0530

    Added Dummy Route

--- app/controllers/HuntController.java
+++ app/controllers/HuntController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import views.html.*;
 import play.mvc.With;
 import java.time.Instant;
-import models.User;
+import models.*;
 import play.mvc.Http.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import java.util.List;
@@ -40,16 +40,26 @@ public class HuntController extends Controller {
         
         //Respond with Bad Request if username/password is not found
         String huntname = payload.findPath("huntname").textValue();
+        JsonNode questionsNode = payload.get("questions");
         
-        JsonNode searchResultsNode = payload.get("questions");
-        
-        for (Object strTemp : searchResultsNode){
-
-            System.out.println(strTemp);
-            
+        List<Question> questionsList = new ArrayList<Question>();
+        for (JsonNode questionNode : questionsNode){
+            //System.out.println(question);
+            String questionText = questionNode.findPath("questiontext").textValue();
+            List<String> options = new ArrayList<String>();
+            JsonNode optionsNode = questionNode.get("options");
+            for (JsonNode option : optionsNode){
+                options.add(option.textValue());
             }
+            Integer answerIndex = Integer.parseInt(questionNode.findPath("questiontext").textValue());
+            
+            Question question = new Question(questionText, options, answerIndex);
+            questionsList.add(question);
+        }
 
-        System.out.println(huntname);
+        Hunt newHunt = new Hunt(huntname, questionsList);
+
+        //System.out.println(huntname);
         
         // Object questions = json.findPath("questions")
 
